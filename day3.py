59b2118ac600fb4d292ece89087c67b69b586314
bijaya-dulal <dulalbijaya0@gmail.com>
2025-05-05 11:05:49 +0545

    drag and drop action

--- day3.py
+++ day3.py
@@ -48,3 +48,14 @@ from selenium.webdriver.common.by import By
 ##to submit the form 
 ## Assume the button has the ID "submit" :)
 #driver.find_element(By.ID, "submit").click()
+
+##Drag and drop 
+## the actionchain class will help to do this 
+
+
+# element = driver.find_element(By.NAME, "source")
+# target = driver.find_element(By.NAME, "target")
+# from selenium.webdriver import ActionChains
+# action_chains = ActionChains(driver)
+# action_chains.drag_and_drop(element, target).perform()
+
