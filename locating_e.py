0c53cfd79404b6b15ffccb3c2007d5d1d21e4a69
bijaya-dulal <dulalbijaya0@gmail.com>
2025-05-06 12:35:18 +0545

    locating methods and technique

---
+++ locating_e.py
@@ -0,0 +1,24 @@
+## this are the methods to select the multiple elements.
+# from selenium.webdriver.common.by import By
+
+# driver.find_element(By.XPATH, '//button[text()="Some text"]')
+# driver.find_elements(By.XPATH, '//button')
+
+##the attributes that can be use with BY class
+
+# ID = "id" #loacte by the id attributes
+# NAME = "name" #locate by the name
+# XPATH = "xpath" #locate by x path , navigate the DOM for example
+ #driver.find_element(By.XPATH, "//input[@type='submit']") #this will locate the input element with type submit
+
+# LINK_TEXT = "link text" # locate anchor by the exact visible text for example
+# diver.find_element(By.LINK_TEXT,'home')
+
+# PARTIAL_LINK_TEXT = "partial link text" #same as the  LINK_TEXT partial match will be okay.
+
+
+# TAG_NAME = "tag name" #locate by the tag name
+# CLASS_NAME = "class name" #by class name
+# CSS_SELECTOR = "css selector" #use css selector
+
+
